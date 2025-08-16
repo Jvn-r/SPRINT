df68284717a73e89e421320d69f050255ebb38a6
Jeevan <rjeevan1495@gmail.com>
2025-08-16 18:54:39 +0530

    The .bat file to get admin access to run wtrace

---
+++ run_wtrace_admin.bat
@@ -0,0 +1,19 @@
+::Need a batch file coz wtrace needs Admin and a batch file seemed like a good way to do it
+@echo off
+setlocal
+
+set CURDIR=%~dp0 
+::This is because when i ran the .bat file without setting directory explicitly, the dir of bat file went through WINDOWS/System32... , and my python file was not in that dir.   
+
+NET SESSION >nul 2>&1
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Requesting administrative privileges
+    powershell -Command "Start-Process '%~f0' -WorkingDirectory '%CURDIR%' -Verb runAs"
+    exit /b
+)
+
+cd /d "%CURDIR%"
+echo Running wtrace with admin rights in %CD%
+wtrace.exe > opfile.txt
+
